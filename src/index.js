192570bc695d8e6e9818e5f7ae4e97699e4c0274
Yasser Salama <yassersalama@Yassers-iMac.local>
2024-07-10 10:13:07 +0300

    adding the homepage

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+import restaurantImage from './pexels-pixabay-260922.jpg';
+
+document.getElementById('hero-image').src = restaurantImage;
+
+console.log('JS is working OK!');
